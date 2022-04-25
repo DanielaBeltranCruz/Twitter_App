d8f9ab38906198f42dfc7ff251e05458feddce74
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-25 15:42:59 -0600

    Actualización de prueba para requerimiento 2

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,12 +2,13 @@ const User = require('./../../app/models/User')
 
 describe("Unit Tests of User Class", () => {
     test('Create an user object', () => {
-        const user = new User(1, "danielabeltrancruz", "Daniela", "Bio", "dataCreated", "lastUpdated")
+        const user = new User(1, "danielabeltrancruz", "Daniela", "Bio")
         expect(user.id).toBe(1)
-        expect(user.username).toBe("danielabeltrancr")
+        expect(user.username).toBe("danielabeltrancruz")
         expect(user.name).toBe("Daniela")
         expect(user.bio).toBe("Bio")
-        expect(user.dataCreated).toBe("dataCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dataCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
+
