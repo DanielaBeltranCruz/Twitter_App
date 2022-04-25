ba5e727437bb5d8b18c6a09e574e62ad3668b239
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-25 16:18:49 -0600

    Actualización de prueba para requerimiento 3

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -7,8 +7,16 @@ describe("Unit Tests of User Class", () => {
         expect(user.username).toBe("danielabeltrancruz")
         expect(user.name).toBe("Daniela")
         expect(user.bio).toBe("Bio")
-        expect(user.dataCreated).not.toBeUndefined()
+        expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined()
+    });
+    
+    test('Add getters', () => {
+        const user = new User(1, "danielabeltrancruz", "Daniela", "Bio")
+        expect(user.getUsername).toBe("danielabeltrancruz")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
     });
 })
 
