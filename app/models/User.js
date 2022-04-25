55f28e777a8ac283d5a90e9963c69749bc40dd53
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-25 16:17:54 -0600

    Actualización para requerimiento 3

--- app/models/User.js
+++ app/models/User.js
@@ -4,8 +4,24 @@ class User{
         this.username = username
         this.name = name
         this.bio = bio
-        this.dataCreated = new Date()
+        this.dateCreated = new Date()
         this.lastUpdated = new Date()
+    }
+
+    get getUsername() {
+        return this.username
+    }
+
+    get getBio() {
+        return this.bio
+    }
+
+    get getDateCreated() {
+        return this.dateCreated
+    }
+
+    get getLastUpdated() {
+        return this.lastUpdated
     }
 }
 
