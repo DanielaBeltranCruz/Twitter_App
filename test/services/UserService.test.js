33a55858820b82a2f0230f2e48e0fcfadc39b44a
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-27 12:59:52 -0600

    Actualización de prueba para requerimiento 2

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -8,4 +8,13 @@ describe("Test for UserService", () => {
         expect(user.name).toBe("Daniela")
         expect(user.bio).not.toBeUndefined()
     });  
+    
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "danielabeltrancruz", "Daniela")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("danielabeltrancruz")
+        expect(userInfoInList[2]).toBe("Daniela")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    });  
 })
