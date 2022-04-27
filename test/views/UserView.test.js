abb55319e701a80e21dad9890f78f21e417c98a2
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-27 15:44:45 -0600

    Actualización de prueba para requerimiento 3

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -12,6 +12,12 @@ describe("Test fot UserView", () => {
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/Necesitan tener un valor valido/)
+    });
+
+    test("Return an error object when try to create a new user with a payload with missing propierties", () =>{
+        const payload = {username: "username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/Necesitan tener un valor valido/)
     })
 })
 
